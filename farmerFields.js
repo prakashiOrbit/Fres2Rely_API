65937cdcafa6c061e08382f31da94f3457d92320
prakshiOrbit <prakashmalu1999@gmail.com>
2022-12-09 18:41:35 +0530

    firstt commit

---
+++ farmerFields.js
@@ -0,0 +1,39 @@
+const farmerFields = {
+  data: [
+    {
+      id: "FID",
+      label: "id",
+      control: "textbox",
+      validation: "alphanumeric",
+      disabled: true,
+    },
+    {
+      id: "Name",
+      label: "name",
+      control: "textbox",
+    },
+    {
+      id: "Address",
+      label: "address",
+      control: "textbox",
+    },
+    {
+      id: "Aadhar",
+      label: "aadhar",
+      control: "textbox",
+    },
+    {
+      id: "PAN",
+      label: "PAN",
+      control: "textbox",
+      validate: "alphanumeric",
+    },
+    {
+      id: "Contact",
+      label: "contact",
+      control: "textbox",
+      validate: "alphanumeric",
+    },
+  ],
+};
+export default farmerFields;
